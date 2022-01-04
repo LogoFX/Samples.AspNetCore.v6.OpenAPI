822b16a6c21fe1bced8d6996192428fda277608f
david <david@logoui.co.il>
2022-01-04 18:31:43 +0400

    feat: add operations

--- src/Samples.AspNetCore.v6.Facade/Controllers/WeatherForecastController.cs
+++ src/Samples.AspNetCore.v6.Facade/Controllers/WeatherForecastController.cs
@@ -5,29 +5,71 @@ namespace Samples.AspNetCore.v6.Facade.Controllers
 	[ApiController]
 	[Route("[controller]")]
 	public class WeatherForecastController : ControllerBase
-	{
-		private static readonly string[] Summaries = new[]
-		{
-		"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-	};
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
 
 		private readonly ILogger<WeatherForecastController> _logger;
 
-		public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        private readonly List<WeatherForecast> _data;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
 		{
-			_logger = logger;
-		}
+            _logger = logger;
+
+            var now = DateTime.Now;
+
+            _data = Enumerable.Range(1, 10).Select(index => new WeatherForecast
+            {
+                Date = now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            }).ToList();
+        }
 
 		[HttpGet(Name = "GetWeatherForecast")]
 		public IEnumerable<WeatherForecast> Get()
 		{
-			return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-			{
-				Date = DateTime.Now.AddDays(index),
-				TemperatureC = Random.Shared.Next(-20, 55),
-				Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-			})
-			.ToArray();
+			return _data.ToArray();
 		}
-	}
+
+        [HttpGet("{date}", Name = "GetWeatherForecastByDate")]
+        public WeatherForecast? GetByDate(DateTime date)
+        {
+            return _data.FirstOrDefault(w => Math.Abs((w.Date.Date - date.Date).TotalDays) == 0);
+        }
+
+        [HttpPost(Name = "PostWeatherForecast")]
+        public void Add([FromBody] WeatherForecast weatherForecast)
+        {
+            _data.Add(weatherForecast);
+        }
+
+        [HttpDelete(Name = "DeleteWeatherForecast")]
+        public void Delete([FromBody] DateTime date)
+        {
+            var wf = GetByDate(date);
+            if (wf != null)
+            {
+                _data.Remove(wf);
+            }
+        }
+
+        [HttpPut(Name = "PutWeatherForecast")]
+        public void Update([FromBody] WeatherForecast weatherForecast)
+        {
+            var wf = GetByDate(weatherForecast.Date);
+            if (wf == null)
+            {
+                Add(weatherForecast);
+            }
+            else
+            {
+                var index = _data.IndexOf(wf);
+                _data[index] = weatherForecast;
+            }
+        }
+    }
 }
